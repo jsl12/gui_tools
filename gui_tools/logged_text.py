3cfc97552ed031e29b57c1a7aea51967abbbbf24
jsl12 <lancaster.js@gmail.com>
2020-11-16 22:31:58 -0600

    initial commit of LoggedText

---
+++ gui_tools/logged_text.py
@@ -0,0 +1,96 @@
+import logging
+import queue
+import tkinter as tk
+import tkinter.ttk as ttk
+from pathlib import Path
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
+
+LOGGER = logging.getLogger(__name__)
+
+
+class LoggedText(ScrolledText):
+    def __init__(self, master, logger='', fmt=None, datefmt=None, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.queue = queue.Queue()
+        self.handler = QueueHandler(self.queue)
+        self.handler.setFormatter(
+            logging.Formatter(
+                fmt=fmt or '%(asctime)s: %(message)s',
+                datefmt=datefmt or '%H:%M:%S'
+            )
+        )
+        logging.getLogger(logger).addHandler(self.handler)
+        self.tag_config('ERROR', foreground='red')
+        self.master.after(100, self.poll_queue)
+
+    def display(self, record):
+        msg = self.handler.format(record)
+        self.configure(state='normal')
+        self.insert(tk.END, msg + '\n', record.levelname)
+        self.configure(state='disabled')
+        self.yview(tk.END)
+
+    def poll_queue(self):
+        while True:
+            try:
+                record = self.queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                self.display(record)
+        self.master.after(100, self.poll_queue)
+
+
+class QueueHandler(logging.Handler):
+    def __init__(self, log_queue):
+        super().__init__()
+        self.log_queue = log_queue
+
+    def emit(self, record):
+        self.log_queue.put(record)
+
+
+class OpenFrame(tk.Frame):
+    def __init__(self, master, *args, **kwargs):
+        kwargs['padx'] = 5
+        kwargs['pady'] = 5
+        super().__init__(master, *args, **kwargs)
+        self.rows = []
+        self.columnconfigure(2, weight=1)
+
+    def add_row(self, label_text, button_text, type = 'file', width=120):
+        if type == 'file':
+            func = filedialog.askopenfilename
+        elif type == 'folder':
+            func = filedialog.askdirectory
+        else:
+            func = type
+
+        new_row = len(self.rows)
+        self.rows.append([
+            ttk.Label(master=self, text=label_text),
+            ttk.Button(master=self, text=button_text, command=lambda *args: self.get_path(func, new_row)),
+            ttk.Entry(master=self, width=width),
+        ])
+        self.rows[-1][0].grid(row=new_row, column=0, sticky=tk.W)
+        self.rows[-1][1].grid(row=new_row, column=1, sticky=tk.W, padx=5)
+        self.rows[-1][2].grid(row=new_row, column=2, sticky=(tk.W, tk.E))
+
+    def get_row_path(self, row_num):
+        try:
+            return Path(self.rows[row_num][-1].get())
+        except:
+            pass
+
+    def get_path(self, func, row_num):
+        self.set_path(row_num,
+                      func(
+                          initialdir=self.get_row_path(row_num) if self.get_row_path(row_num).is_dir() else self.get_row_path(row_num).parents[0]
+                      ))
+
+    def set_path(self, row_num, path):
+        path = Path(path) if not isinstance(path, Path) else path
+        text_box = self.rows[row_num][2]
+        text_box.delete(0, len(text_box.get()))
+        text_box.insert(0, str(path))
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gui_tools',
+    version='0.1',
+    description='Collection of helpers for making GUIs',
+    author='John Lancaster',
+    author_email='lancaster.js@gmail.com',
+    # install_requires=[
+    #     'tkinter'
+    # ],
+    packages=find_packages()
+)
