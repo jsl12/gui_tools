b83e38f0b559da7523d3abe5192a922a5f62f3e8
jsl12 <lancaster.js@gmail.com>
2020-12-12 00:48:37 -0600

    ModalInput overhaul

--- gui_tools/modal.py
+++ gui_tools/modal.py
@@ -4,63 +4,62 @@ from tkinter import ttk as ttk
 
 class ModalInput(tk.Toplevel):
     def __init__(self, master,
-                 width: int = 50, title: str = None,
+                 title: str = None,
                  default: str = None,
                  takefocus=True,
                  offset=(100, 100),
                  padding: int = 10,
+                 # show: bool = False,
                  *args, **kwargs):
         super().__init__(master=master,
                          takefocus=takefocus,
                          *args, **kwargs)
+        self.padding = padding
+        self.default = default
+        self.offset = offset
+
         self.withdraw()
         self.resizable(False, False)
+
         if title is not None:
             self.title(title)
 
-        self.entry = ttk.Entry(master=self, width=width)
+        self.create_input()
+        self.create_buttons()
 
+        self.deiconify()
+
+    def create_input(self):
+        tk.Frame(master=self).pack()
+
+    def create_buttons(self):
         button_frame = ttk.Frame(master=self)
 
-        self.ok_button = ttk.Button(master=button_frame,
-                                    text='OK',
-                                    command=lambda: self.destroy(set_val=True))
-        self.ok_button.pack(side=tk.LEFT, padx=(0, int(padding/2)))
-        self.cancel_button = ttk.Button(master=button_frame,
-                                        text='Cancel',
-                                        command=lambda: self.destroy())
-        self.cancel_button.pack(side=tk.LEFT,
-                                padx=(int(padding/2), 0))
+        ok_button = ttk.Button(master=button_frame,
+                               text='OK',
+                               command=lambda: self.destroy(set_val=True))
+        cancel_button = ttk.Button(master=button_frame,
+                                   text='Cancel',
+                                   command=lambda: self.destroy(set_val=False))
 
-        self.entry.pack(side=tk.TOP,
-                        fill=tk.BOTH,
-                        padx=padding, pady=padding)
-        if (frame := self.insert_frame()) is not None:
-            frame.pack(side=tk.TOP)
-        button_frame.pack(side=tk.TOP, padx=padding, pady=padding)
+        ok_button.pack(side=tk.LEFT, padx=(0, int(self.padding / 2)))
+        cancel_button.pack(side=tk.LEFT,
+                           padx=(int(self.padding / 2), 0))
 
-        self.set_rel_position(offset)
-        self.deiconify()
+        button_frame.pack(side=tk.TOP,
+                          padx=self.padding, pady=self.padding)
+
+    def destroy(self, set_val: bool = False):
+        self.grab_release()
+        super().destroy()
+
+    def deiconify(self):
+        self.set_rel_position(self.offset)
+        super().deiconify()
         self.transient(self.master)
         self.lift()
         self.grab_set()
 
-        if default is not None:
-            self.value = default
-            self.entry.delete(0, tk.END)
-            self.entry.insert(0, default)
-
-        self.entry.focus()
-
-    def destroy(self, set_val: bool = False):
-        self.grab_release()
-        if set_val:
-            self.value = self.entry.get()
-        super().destroy()
-
     def set_rel_position(self, offset):
         self.update_idletasks()
         self.geometry(f'+{self.master.winfo_x() + offset[0]}+{self.master.winfo_y() + offset[1]}')
-
-    def insert_frame(self) -> tk.Frame:
-        pass
